0de2061070f81176a4952b6de93a9b31dcfc9bcf
TTC <theomaniac917@gmail.com>
2025-08-28 23:24:08 +0300

    hello and var

---
+++ hello_rust/src/main.rs
@@ -0,0 +1,12 @@
+// cargo init PROJECT_NAME
+// cargo build
+// cargo fmt
+// cargo run
+
+fn main() {
+    println!("Hello, world!");
+
+    println!("Hello, world!");
+
+    println!("Hello, world!");
+}
