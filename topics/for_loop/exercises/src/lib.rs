c3c7560160004f36c1c31f1c303cb2eca9830c0b
TTC <theomaniac917@gmail.com>
2025-09-02 23:57:26 +0300

    for loop

--- topics/for_loop/exercises/src/lib.rs
+++ topics/for_loop/exercises/src/lib.rs
@@ -1,7 +1,15 @@
 pub fn sum(nums: Vec<i32>) -> i32 {
-    todo!();
+    let mut sum: i32 = 0;
+    for num in nums {
+        sum += num;
+    }
+    sum
 }
 
 pub fn fill(i: u32, n: usize) -> Vec<u32> {
-    todo!();
+    let mut vec: Vec<u32> = Vec::new();
+    for _ in 0..n {
+        vec.push(i);
+    }
+    vec
 }
