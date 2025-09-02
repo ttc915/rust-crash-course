5b696517f2e272ed5311cae5ed8e87f4df4a24a6
TTC <theomaniac917@gmail.com>
2025-09-02 17:23:09 +0300

    Struct

--- topics/struct_type/exercises/src/lib.rs
+++ topics/struct_type/exercises/src/lib.rs
@@ -1,6 +1,15 @@
+#![allow(unused)]
+
 #[derive(Debug)]
-pub struct Account {}
+pub struct Account {
+    pub address: String,
+    pub balance: u32,
+}
 
 pub fn new(address: String) -> Account {
-    todo!();
+    let account: Account = Account {
+        address,
+        balance: 0,
+    };
+    account
 }
