8824c69155dc41096b8a8e598b56e6da1f02e18a
TTC <theomaniac917@gmail.com>
2025-09-03 16:05:11 +0300

    unwrap

--- topics/unwrap/exercises/src/lib.rs
+++ topics/unwrap/exercises/src/lib.rs
@@ -1,7 +1,11 @@
+#![allow(unused)]
+
 pub fn parse_and_add(a: &str, b: &str) -> u32 {
-    todo!();
+    let a: u32 = a.parse().expect("Failed to parse variable");
+    let b: u32 = b.parse().expect("Failed to parse variable");
+    a + b
 }
 
 pub fn unwrap_and_add(x: Option<u32>, y: Option<u32>) -> u32 {
-    todo!();
+    x.unwrap() + y.unwrap()
 }
