5be40bb2b233ea5b243997f545ddeb97a13a4981
TTC <theomaniac917@gmail.com>
2025-09-03 12:34:32 +0300

    match

--- topics/pattern_match/exercises/src/lib.rs
+++ topics/pattern_match/exercises/src/lib.rs
@@ -1,7 +1,16 @@
 pub fn num_to_string(num: u32) -> String {
-    todo!();
+    match num {
+        0 => "zero".to_string(),
+        1 => "one".to_string(),
+        2 => "two".to_string(),
+        3 => "three".to_string(),
+        _ => "other".to_string(),
+    }
 }
 
 pub fn unwrap_or_default(x: Option<u32>, v: u32) -> u32 {
-    todo!();
+    match x {
+        Some(x) => x,
+        None => v,
+    }
 }
