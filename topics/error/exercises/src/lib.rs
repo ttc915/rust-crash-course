ad523776523f2260f4ecbe99ff87b852370c5d29
TTC <theomaniac917@gmail.com>
2025-09-03 15:57:47 +0300

    error

--- topics/error/exercises/src/lib.rs
+++ topics/error/exercises/src/lib.rs
@@ -4,9 +4,17 @@ pub enum MathError {
 }
 
 pub fn div(x: u32, y: u32) -> Result<u32, MathError> {
-    todo!();
+    if y == 0 {
+        Err(MathError::DivByZero)
+    } else {
+        Ok(x / y)
+    }
 }
 
 pub fn get(v: &[u32], i: usize, default_val: u32) -> u32 {
-    todo!();
+    if i < v.len() {
+        v[i]
+    } else {
+        default_val
+    }
 }
