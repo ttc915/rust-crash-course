19968b5beb218a0083de9c09d0dce99d7b8941f8
TTC <theomaniac917@gmail.com>
2025-09-07 15:05:18 +0300

    Generic types

--- topics/generic_type/exercises/src/lib.rs
+++ topics/generic_type/exercises/src/lib.rs
@@ -1,15 +1,15 @@
-pub fn first(t: (u32, i32)) -> u32 {
+pub fn first<A, B>(t: (A, B)) -> A {
     t.0
 }
 
-pub fn last(t: (u32, i32)) -> i32 {
+pub fn last<A, B>(t: (A, B)) -> B {
     t.1
 }
 
 #[derive(Debug)]
-pub struct Rectangle {
-    pub top: u32,
-    pub left: u32,
-    pub width: u32,
-    pub height: u32,
+pub struct Rectangle<T> {
+    pub top: T,
+    pub left: T,
+    pub width: T,
+    pub height: T,
 }
